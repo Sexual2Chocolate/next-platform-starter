cdef6416b8e77816c1120e0e6f48d9c97a5093f2
Sexual2Chocolate <98413317+Sexual2Chocolate@users.noreply.github.com>
2024-06-13 05:42:32 +0100

    Update walletModal.tsx

--- app/walletPage/walletModal.tsx
+++ app/walletPage/walletModal.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import Modal from 'react-modal';
 import '../wallet.css';
-import '../about/NftCard.css';
+import '../about/NftCard.module.css';
 interface WalletModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
