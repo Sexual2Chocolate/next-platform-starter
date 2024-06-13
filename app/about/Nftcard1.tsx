b29cfa7a300dc090d1869585bda44b24859ce029
Sexual2Chocolate <98413317+Sexual2Chocolate@users.noreply.github.com>
2024-06-13 05:45:23 +0100

    Update Nftcard1.tsx

--- app/about/Nftcard1.tsx
+++ app/about/Nftcard1.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import WalletModal from '../walletPage/walletModal';
-import './NftCard.module.css';
+import './Nftcard.module.css';
 interface nfts {
   imgUrl: any;
   title?: string;
